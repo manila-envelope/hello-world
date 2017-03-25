91c22eaf88f4d03e9345afaab9b3cc8670a696ec
manila-envelope <jberr011@odu.edu>
2017-03-25 10:10:09 -0400

    Example of retry/until with cmdlets that have no error action.

---
+++ nonTerminating.ps1
@@ -0,0 +1,39 @@
+﻿# Ensure non-terminating errors are stopped
+$currentErrorActionPreference = $ErrorActionPreference
+$ErrorActionPreference = "Stop"
+
+$retryCount = 0
+$stopLoop = $false
+
+do {
+
+    try {
+
+        ### Command you want to execute, error action must = stop
+        Copy-Item c:\missingFolder c:\missingFolder2
+        Write-Host "Connection was successful."
+        $stopLoop = $true
+        
+    } catch {
+
+        if ($retryCount -gt 5) {
+
+        ### End do/while
+        throw "Could not connect."
+        $stopLoop = $true
+            
+        } else {
+            
+            ### Wait 30 seconds, try again
+            Write-Host "Could not connect, retrying in 5 seconds."
+            Start-Sleep 5
+            $retryCount++
+
+        }
+
+    }
+        
+} while ($stopLoop -eq $false)
+
+# Reset $ErrorActionPreference to prior value
+$ErrorActionPreference = $currentErrorActionPreference
